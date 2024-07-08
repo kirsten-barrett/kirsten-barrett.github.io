ec01b7ec54ac0b8b2266f4009f7643c862d02a0b
kirstenbarrett <kirsten.barrett25@gmail.com>
2024-07-08 18:43:29 -0400

    Rearrange projects & edit bio title

--- script/typeWriter.js
+++ script/typeWriter.js
@@ -1,6 +1,7 @@
 var typed = new Typed("#auto-type", {
     stringsElement: "#typed-strings",
     typeSpeed: 100,
-    backSpeed: 150,
+    backSpeed: 200,
+    backDelay: 2000,
     loop: true,
 })
