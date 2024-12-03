43bf592f87207b238381fd66c49c23581a19e886
kirsten-barrett <kirsten.barrett25@gmail.com>
2024-12-03 09:29:06 -0500

    HHF case study layout and content update

--- script/emailButton.js
+++ script/emailButton.js
@@ -3,10 +3,3 @@ function copyEmail() {
     navigator.clipboard.writeText("kirsten.barrett25@gmail.com");
     alert("'kirsten.barrett25@gmail.com' copied to clipboard!");
 }
-
-/* TWO LINE FOOTER ON MOBILE */
-const copyright = document.querySelector('#copyright')
-if (window.screen.width < 470) {
-    copyright.innerHTML = "© 2024 <br>Kirsten Barrett";
-}
-
